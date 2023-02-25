49efb2361f32fc6389dc524fef44fc89a0eccbae
Pizik Ilya <ilya.pizik89@gmail.com>
2023-02-25 17:21:28 +0300

    feat: add ddd shared classes
    DDD classes implementation taken from khalilstemmler.com

---
+++ src/shared/domain/Repository.ts
@@ -0,0 +1,9 @@
+import { UniqueEntityID } from "./UniqueEntityID";
+
+export interface Repository<T> {
+  exists(t: T): Promise<boolean>;
+  delete(t: T): Promise<any>;
+  save(t: T): Promise<any>;
+
+  getNewId(): UniqueEntityID;
+}
