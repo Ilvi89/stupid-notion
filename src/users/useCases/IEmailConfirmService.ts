72f9d1e16cebe4ae073abfe1fc4c0df2d801cd9a
Pizik Ilya <ilya.pizik89@gmail.com>
2023-03-20 22:16:20 +0300

    refactor: email confirm service

--- src/users/useCases/IEmailConfirmService.ts
+++ src/users/useCases/IEmailConfirmService.ts
@@ -1,13 +1,7 @@
 import { Email } from "../domain/valueObjects/Email";
 
-export interface ConfirmCode {
-  value: string;
-  createdAt: Date;
-}
-
 
 export interface IEmailConfirmService {
   requestConfirm(email: Email): void;
-
-  getConfirmCode(email: Email): Promise<ConfirmCode>;
+  getConfirmCode(email: Email): Promise<string>;
 }
