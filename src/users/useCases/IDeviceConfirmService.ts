12c7cb9c4b8d0a1f4308326de8ceab42bd603e80
Pizik Ilya <ilya.pizik89@gmail.com>
2023-03-22 00:06:46 +0300

    feet: add confirm access use case

--- src/users/useCases/IDeviceConfirmService.ts
+++ src/users/useCases/IDeviceConfirmService.ts
@@ -2,4 +2,5 @@ import { Device } from "../domain/valueObjects/Session";
 
 export interface IDeviceConfirmService {
   requestConfirm(currentlyInUseDevices: Device[]): void;
+  getConfirmCode(device: Device): Promise<string>;
 }
--- src/users/useCases/confirmAccess.ts
+++ src/users/useCases/confirmAccess.ts
@@ -1,8 +1,8 @@
 import { UniqueEntityID } from "../../shared/domain/UniqueEntityID";
 import { UseCase } from "../../shared/domain/UseCase";
 import { IUserRepo } from "../domain/repositories/IUserRepo";
-import { IEmailCheckService } from "./IEmailVerifyService";
-import { IDeviceCheckService } from "./IDeviceCheckService";
+import { IEmailConfirmService } from "./IEmailConfirmService";
+import { IDeviceConfirmService } from "./IDeviceConfirmService";
 import { UserAggregate } from "../domain/userAggregate";
 import { Device } from "../domain/valueObjects/Session";
 
@@ -10,6 +10,7 @@ interface ConfirmAccessDTO {
   type: ConfirmAccessType;
   userId: UniqueEntityID;
   deviceId: string;
+  code: string;
 }
 
 export enum ConfirmAccessType {
@@ -20,39 +21,32 @@ export enum ConfirmAccessType {
 type Response = void
 
 export class ConfirmAccess implements UseCase<ConfirmAccessDTO, Response> {
-  private userRepo: IUserRepo;
-  private emailCheckService: IEmailCheckService;
-  private deviceCheckService: IDeviceCheckService;
 
-  constructor(userRepo: IUserRepo, emailCheckService: IEmailCheckService, deviceCheckService: IDeviceCheckService) {
-    this.userRepo = userRepo;
-    this.emailCheckService = emailCheckService;
-    this.deviceCheckService = deviceCheckService;
+  constructor(
+    private readonly userRepo: IUserRepo,
+    private readonly emailConfirmService: IEmailConfirmService,
+    private readonly deviceConfirmService: IDeviceConfirmService) {
   }
 
   async execute(req: ConfirmAccessDTO): Promise<Response> {
     const user: UserAggregate = await this.userRepo.find(req.userId);
-    if (!user)
-      throw new Error("User not found");
+    if (!user) throw new Error("User not found");
 
-    // Todo: refactor this shit
     const device = Device.create({ id: req.deviceId });
+    let cc: string;
+    // todo: ?split this to another use cases
     if (req.type == ConfirmAccessType.Email) {
-      if (!user.isEmailVerified) {
-        throw new Error("User email is unconfirmed");
-      }
-      if (await this.emailCheckService.isConfirmed(user.email)) {
-        user.createNewSession(device);
-        await this.userRepo.save(user);
-      }
+      if (!user.isEmailVerified) throw new Error("User email is unconfirmed");
+      cc = await this.emailConfirmService.getConfirmCode(user.email);
     } else {
-      if (user.currentlyInUseDevices.length == 0) {
-        throw new Error("There are currently no devices in use");
-      }
-      if (await this.deviceCheckService.isConfirmed(device)) {
-        user.createNewSession(device);
-        await this.userRepo.save(user);
-      }
+      if (user.currentlyInUseDevices.length == 0) throw new Error("There are currently no devices in use");
+      cc = await this.deviceConfirmService.getConfirmCode(device);
     }
+
+    if (!cc || cc != req.code) throw new Error("Access is unconfirmed");
+    user.createNewSession(device);
+    await this.userRepo.save(user);
   }
+
+
 }
