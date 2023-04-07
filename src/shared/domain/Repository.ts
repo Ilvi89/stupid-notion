1b8c09fd1c0a7cc63abb6390087b2b65a63ef688
Pizik Ilya <ilya.pizik89@gmail.com>
2023-04-08 01:01:47 +0300

    refactor: split base repository interface

--- src/shared/domain/Repository.ts
+++ src/shared/domain/Repository.ts
@@ -1,8 +1,12 @@
 import { UniqueEntityID } from "./UniqueEntityID";
 
-export interface Repository<T> {
+export interface ReadRepository<T> {
   find(id: UniqueEntityID): Promise<T>
   exists(t: T): Promise<boolean>;
+}
+
+export interface Repository<T> extends ReadRepository<T>{
   save(t: T): Promise<T>;
   getNewId(): UniqueEntityID;
 }
+
